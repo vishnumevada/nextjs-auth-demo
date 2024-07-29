72194f4dc755a25aaca08b2cc5bfb9ef6d2a2d01
vishnu mevada <vishnu.mevada@txtech.co>
2024-07-29 18:09:43 +0530

    add profile page route '/'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 "use client";
 import Link from "next/link";
+import ProfilePage from "./profile/page";
 
 export default function Home() {
     return (
-        <div className="flex justify-center h-screen m-auto mx-auto">
-            <div className="mt-16">
-                <p className="my-10">Home page</p>
-                <Link href={"/login"}>
-                    <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-                        Login
-                    </button>
-                </Link>
-            </div>
+        <div>
+            <ProfilePage />
         </div>
     );
 }
