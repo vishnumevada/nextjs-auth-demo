0eecc1cd749924d153184bf6a7b4587ead86b3db
vishnu mevada <vishnu.mevada@txtech.co>
2024-07-30 10:40:04 +0530

    Fixed cookies bugs on logout api

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,10 +7,18 @@ export async function GET() {
             success: true,
         });
 
+        // Ensure the cookie is removed
         response.cookies.set("token", "", {
             httpOnly: true,
             expires: new Date(0),
+            path: "/", // Ensure the path is correctly set
+            domain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN || undefined, // Set the domain if needed
         });
+
+        // response.cookies.set("token", "", {
+        //     httpOnly: true,
+        //     expires: new Date(0),
+        // });
 
         return response;
     } catch (error: any) {
