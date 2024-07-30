6fbe059f6ca3cb629a6e78b63cf7d52b3cc10e3d
vishnu mevada <vishnu.mevada@txtech.co>
2024-07-30 10:27:53 +0530

    set middleware path /profile

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -6,10 +6,12 @@ export async function GET() {
             message: "Logout successful",
             success: true,
         });
+
         response.cookies.set("token", "", {
             httpOnly: true,
             expires: new Date(0),
         });
+
         return response;
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
