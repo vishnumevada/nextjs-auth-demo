1ba2ad01f6e9c78bf1acfd69b99fb055510489fb
vishnu mevada <vishnu.mevada@txtech.co>
2024-08-02 11:46:53 +0530

    Image height width set

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -70,18 +70,18 @@ const Header = () => {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex flex-shrink-0 items-center">
-                            <Image
+                            {/* <Image
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                                 alt="Your Company"
                                 width={500}
                                 height={300}
                                 className="h-8 w-auto"
-                            />
-                            {/* <img
+                            /> */}
+                            <img
                                 alt="Your Company"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                                 className="h-8 w-auto"
-                            /> */}
+                            />
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
