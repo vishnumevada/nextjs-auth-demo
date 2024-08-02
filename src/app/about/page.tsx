20220b9515b7569085eb007d3723c129b38d5efb
vishnu mevada <vishnu.mevada@txtech.co>
2024-08-02 17:31:15 +0530

    fix about page bg css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,11 +23,16 @@ const AboutPage = () => {
         <>
             <Header />
             <div className="flex-grow relative isolate overflow-hidden bg-white py-52">
-                <Image
+                {/* <Image
                     src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
                     alt="Background"
                     width={500}
                     height={300}
+                    className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+                /> */}
+                <img
+                    src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                    alt="Background"
                     className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
                 />
                 <div
